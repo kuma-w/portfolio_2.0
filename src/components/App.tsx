4d0b544fcb30256cccbc1f615227125a90aa55aa
kuma-w <occhoi90@gmail.com>
2022-10-19 16:36:42 +0900

    new : 스크롤로 모니터 좌우 이동

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,17 @@
-import React from "react";
-import Monitor from "./monitor";
+import React, { useState, useEffect } from "react";
+import Monitor, { Point, Size } from "./monitor";
+import useScroll from "../hooks/useScroll";
 
 const App = () => {
+  const { scrollY } = useScroll();
+  const point1: Point = { x: 100 + scrollY, y: 100 };
+  const point2: Point = { x: 800 + scrollY, y: 100 };
+  const size: Size = { width: 515, height: 300 };
+
   return (
-    <div>
-      <Monitor />
+    <div style={{ width: "100%", height: "100%", position: "absolute" }}>
+      <Monitor pos={point1} size={size} />
+      <Monitor pos={point2} size={size} />
     </div>
   );
 };
--- src/components/monitor.tsx
+++ src/components/monitor.tsx
@@ -2,52 +2,72 @@ import React from "react";
 import { styled } from "@mui/material/styles";
 import { Box } from "@mui/material";
 
-const MyMonitor = styled(Box)(() => ({
+export interface MonitorProps {
+  pos?: Point;
+  size?: Size;
+}
+
+export interface Point {
+  x: number;
+  y: number;
+}
+
+export interface Size {
+  width: number;
+  height: number;
+}
+
+const MyMonitor = styled(Box, {
+  shouldForwardProp: (prop) => prop !== "pos" && prop !== "size",
+})<MonitorProps>(({ pos, size }) => ({
   ".monitor": {
-    width: "515px",
-    height: "300px",
-    overflowY: "scroll",
+    position: "fixed",
+    width: `${size?.width}px`,
+    height: `${size?.height}px`,
+    // overflowY: "scroll",
     border: "solid 1em #333",
     borderRadius: ".5em",
+    left: `${pos?.x}px`,
+    top: `${pos?.y}px`,
   },
 
-  ".monitor::-webkit-scrollbar": {
-    width: "15px",
-  },
-  ".monitor::-webkit-scrollbar-thumb": {
-    background: "#666",
-  },
-  "::-webkit-scrollbar-track": {
-    backgroundColor: "#888",
-  },
+  //   ".monitor::-webkit-scrollbar": {
+  //     width: "15px",
+  //   },
+  //   ".monitor::-webkit-scrollbar-thumb": {
+  //     background: "#666",
+  //   },
+  //   "::-webkit-scrollbar-track": {
+  //     backgroundColor: "#888",
+  //   },
 
-  ".stand:before": {
-    content: '""',
-    display: "block",
-    position: "relative",
-    background: "#222",
-    width: "150px",
-    height: "50px",
-    top: "382px",
-    left: "185px",
-  },
-  ".stand:after": {
-    content: '""',
-    display: "block",
-    position: "relative",
-    background: "#333",
-    borderTopLeftRadius: ".5em",
-    borderTopRightRadius: ".5em",
-    width: "300px",
-    height: "15px",
-    top: "50px",
-    left: "110px",
-  },
+  //   ".stand:before": {
+  //     content: '""',
+  //     display: "block",
+  //     position: "relative",
+  //     background: "#222",
+  //     width: "150px",
+  //     height: "50px",
+  //     top: "382px",
+  //     left: "185px",
+  //   },
+  //   ".stand:after": {
+  //     content: '""',
+  //     display: "block",
+  //     position: "relative",
+  //     background: "#333",
+  //     borderTopLeftRadius: ".5em",
+  //     borderTopRightRadius: ".5em",
+  //     width: "300px",
+  //     height: "15px",
+  //     top: "50px",
+  //     left: "110px",
+  //   },
 }));
 
-const monitor = () => {
+const monitor = ({ pos, size }: MonitorProps) => {
   return (
-    <MyMonitor>
+    <MyMonitor pos={pos} size={size}>
       <Box className="stand">
         <Box className="monitor"></Box>
       </Box>
